a370cebbbf7d69c85a53d8ca850e57553a442bd2
tsott <wedlepotrzeby@gmail.com>
2012-12-30 20:34:44 +0100

    szkielet aplikacji dzialajacy na heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'e06c9dc7d8240c1f152af4d6a624626377e9f47c863941637e98a8c917535f972b6fad0ccbd1dbe4995d45d97e1aa96dedc75a970b27c78a71a10f06a225e2b8'
